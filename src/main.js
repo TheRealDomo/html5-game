7b0aacb211db54e65b899ba618b21600cb46a2cb
TheRealDomo <ajcisse474@gmail.com>
2024-12-02 09:43:18 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,18 @@
+let randomInteger = Math.floor(Math.random() * 20) + 1;
+let guess;
+let guesses = 0;
 
+
+while (guess != randomInteger) {
+  guess = parseInt(prompt("What number am I thinking of?"), 10);
+  if (guess === randomInteger) {
+    break;
+  }
+  guesses++;
+  if (guess < randomInteger) {
+    alert("Higher!")
+  }else{
+    alert("Lower")
+  }
+}
+alert(`Good Job! You got it in ${guesses} guesses!`)
