d91b1f9d1cf1814c100ed77cc3b0c1d492eedaf3
TheRealDomo <ajcisse474@gmail.com>
2024-12-02 10:46:06 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -6,15 +6,30 @@ const input = document.querySelector("input");
 ctx.fillStyle = "green";
 
 
-input.addEventListener("keydown",processInput);
+input.addEventListener(
+  "keydown",
+  (event) => {
+    if (event.defaultPrevented) {
+      return; // Do nothing if event already handled
+    }
 
-function processInput(e) {
-  switch e.code {
-    case "ArrowDown" :
-      alert("Down Arrow Pressed")
-  }
-    
-}
+    switch (event.code) {
+      case "KeyS":
+      case "ArrowDown":
+          alert("Down keypress")               
+      case "KeyW":
+      case "ArrowUp":
+        alert("Up keypress")               
+      case "KeyA":
+      case "ArrowLeft":
+        alert("Left keypress")        
+      case "KeyD":
+      case "ArrowRight":
+        alert("Right keypress")
+    }
+,
+  true,
+);
 
 function mainLoop() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
