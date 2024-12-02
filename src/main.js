c91fdcacfe66877e08812a225048e1c7892f8b28
TheRealDomo <ajcisse474@gmail.com>
2024-12-02 09:56:34 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,18 +1,5 @@
-let randomInteger = Math.floor(Math.random() * 20) + 1;
-let guess;
-let guesses = 0;
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
-
-while (guess != randomInteger) {
-  guess = parseInt(prompt("What number am I thinking of?"), 10);
-  if (guess === randomInteger) {
-    break;
-  }
-  guesses++;
-  if (guess < randomInteger) {
-    alert("Higher!")
-  }else{
-    alert("Lower")
-  }
-}
-alert(`Good Job! You got it in ${guesses} guesses!`)
+ctx.fillStyle = "green";
+ctx.fillRect(10, 10, 150, 100);
