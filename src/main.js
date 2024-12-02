8f99a9bd56556b20d21217fe2ec59ebbf600d844
TheRealDomo <ajcisse474@gmail.com>
2024-12-02 17:23:41 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ const ctx = canvas.getContext("2d");
 const { width: w, height: h } = canvas;
 let dt = 0;
 let last = 0;
-//const keyboardInput = new keyControls();
+const keyboardInput = new keyControls();
 const speed = 64;
 let p1 = 0;
 
@@ -18,8 +18,9 @@ function mainLoop(ms) {
   ctx.fillRect(0, 0, w, h);
   ctx.strokeText(`Frame length: ${(dt * 1000).toFixed(2)} ms`, 70, 50);
   ctx.strokeText(`Total time: ${t.toFixed(2)}`, 70, 90);
-
-  p1 += speed * dt;
+  if (keyboardInput.action) {
+    p1 += (speed * dt) * controls.y;
+  }
   if (p1 > w) p1 -= w + 50;
   ctx.fillStyle = "red";
   ctx.fillRect(p1, 120, 50, 50);
