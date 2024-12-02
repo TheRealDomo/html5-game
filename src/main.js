bee648c881a2315fb2ca6e1374248dc3697ae5ae
TheRealDomo <ajcisse474@gmail.com>
2024-12-02 16:59:32 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,39 +1,29 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
+let dt = 0;
+let last = 0;
 const keyboardInput = new keyControls();
+const speed = 64;
+let p1 = 0;
 
 
-ctx.fillStyle = "green";
+function mainLoop(ms) {
+  requestAnimationFrame(mainLoop);
+  const t = ms / 1000;
+  dt = t - last;
+  last = t;
 
+  ctx.fillStyle = "#000";
+  ctx.fillRect(0, 0, w, h);
+  ctx.strokeText(`Frame length: ${(dt * 1000).toFixed(2)} ms`, 70, 50);
+  ctx.strokeText(`Total time: ${t.toFixed(2)}`, 70, 90);
 
-input.addEventListener(
-  "keydown",
-  (event) => {
-    if (event.defaultPrevented) {
-      return; // Do nothing if event already handled
-    }
-
-    switch (event.code) {
-      case "KeyS":
-      case "ArrowDown":
-          alert("Down keypress")               
-      case "KeyW":
-      case "ArrowUp":
-        alert("Up keypress")               
-      case "KeyA":
-      case "ArrowLeft":
-        alert("Left keypress")        
-      case "KeyD":
-      case "ArrowRight":
-        alert("Right keypress")
-    }
-,
-  true,
-);
-
-function mainLoop() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  
+  p1 += speed * dt;
+if (p1 > w) p1 -= w + 50;
+ctx.fillStyle = "#f00";
+ctx.fillRect(p1, 120, 50, 50);
 }
 
-setInterval(mainLoop(),30);
+requestAnimationFrame(mainLoop);
+
+
