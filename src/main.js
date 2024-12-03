cd29219904864caf9d2078fb18b2fd7abc651a49
TheRealDomo <ajcisse474@gmail.com>
2024-12-03 08:56:11 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import KeyControls from "../lib/KeyControl.js"
+import KeyControls from "../lib/keyControls.js"
 const canvas = document.querySelector("#board canvas");
 const ctx = canvas.getContext("2d");
 const { width: w, height: h } = canvas;
