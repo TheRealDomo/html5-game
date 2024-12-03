1b9ffba0bf62145dfd43d136d2715d8d606bd59b
TheRealDomo <ajcisse474@gmail.com>
2024-12-03 10:32:56 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,26 @@
-import KeyControls from "../lib/keyControls.js"
+class keyControls{
+  constructor () {
+    this.keys = {};
+    document.addEventListener("keyDown", e =>{
+      if ([37,38,39,40].indexOf(e.which) >= 0) {
+        e.preventDefault();
+      }
+      this.keys[e.which] = true;
+    },false);
+    document.addEventListener("keyup", e => {
+      this.keys[e.which] = false;
+    }, false);
+  }
+  get y() {
+    if (this.keys[38] || this.keys[87]) {
+      return -1;
+    }
+    if (this.keys[40] || this.keys[83]) {
+      return 1;
+    }
+    return 0;
+  }
+}
 const canvas = document.querySelector("#board canvas");
 const ctx = canvas.getContext("2d");
 const { width: w, height: h } = canvas;
